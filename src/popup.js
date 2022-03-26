3d47200cc1bdc5e5d9adf96f5ce3d1e76f421ced
mhanray <mhanray@gmail.com>
2022-03-26 11:58:21 -0700

    added default links

--- src/popup.js
+++ src/popup.js
@@ -1,32 +1,36 @@
+const DEFAULT_LINKS = [`https://placekitten.com/${(Math.floor(Math.random()*10)+1) * 100}/${(Math.floor(Math.random()*10)+1) * 100}`, `https://placedog.net/${(Math.floor(Math.random()*10)+1) * 100}`]
+
 function randomLink(e) {
-    chrome.storage.sync.get('storedLinks', (data) => {
+    chrome.storage.sync.get({'storedLinks': DEFAULT_LINKS}, (data) => {
         var link = data.storedLinks[Math.floor(Math.random()*data.storedLinks.length)];
         console.log(link);
         chrome.tabs.create({url: link});
-        //window.close(); // Note: window.close(), not this.close()
     })
 }
 
 function addLinks(e) { 
     const url = document.getElementById('linkInput').value;
-    //chrome.storage.sync.clear();
-    chrome.storage.sync.get('storedLinks', (data) => {
-        //console.log(data);
+    chrome.storage.sync.get({'storedLinks': DEFAULT_LINKS}, (data) => {
         storedLinks = data.storedLinks;
-        if (storedLinks) {
-            storedLinks.push(url);
-        } else {
-            storedLinks = [url];
-        }
-        console.log(storedLinks);
 
-        chrome.storage.sync.set({'storedLinks':storedLinks});  
+        if (url!= '') {
+            if (storedLinks) {
+                storedLinks.push(url);
+            } else {
+                storedLinks = [url];
+            }
+        }
+        chrome.storage.sync.set({'storedLinks':storedLinks});
+
+        console.log(storedLinks);
     })
     //window.close();
+
 }
 
 function resetLinks (e) {
     chrome.storage.sync.clear();
+    console.log(`storage reset`);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
