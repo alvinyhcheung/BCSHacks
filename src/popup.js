5d3bc12d54dd133f2a818a9cee379a4dc973f126
mhanray <mhanray@gmail.com>
2022-03-26 11:04:16 -0700

    temp working

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,31 @@
-function redirectHandler(e) {
-    chrome.tabs.create({url: "https://google.com"});
-    window.close(); // Note: window.close(), not this.close()
+function randomLink(e) {
+    chrome.storage.sync.get('storedLinks', (data) => {
+        var link = data.storedLinks[Math.floor(Math.random()*data.storedLinks.length)];
+        chrome.tabs.create({url: "https://google.com"});
+        window.close(); // Note: window.close(), not this.close()
+    })
+}
+
+function addLink(e) { 
+    console.log()
+    chrome.storage.sync.clear();
+    chrome.storage.sync.get('storedLinks', (data) => {
+        storedLinks = data.storedLinks;
+        if (storedLinks) {
+            storedLinks.push(e.targetvalue);
+        } else {
+            storedLinks = [e.targetvalue];
+        }
+        console.log(storedLinks);
+
+        chrome.storage.sync.set({'storedLinks':storedLinks});  
+    })
+    //window.close();
 }
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('google').addEventListener('click', redirectHandler);
+    document.getElementById('redirect').addEventListener('click', randomLink);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('submitLink').addEventListener('click', addLink);
 });
