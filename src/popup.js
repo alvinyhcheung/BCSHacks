3faa7df2955cdd17fc26f06da088c4dddb1d8a1f
mhanray <mhanray@gmail.com>
2022-03-26 17:11:57 -0700

    add interrupts functionality

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,9 @@
 const DEFAULT_LINKS = [
     `https://placekitten.com/${(Math.floor(Math.random()*10)+1) * 100}/${(Math.floor(Math.random()*10)+1) * 100}`,
     `https://placedog.net/${(Math.floor(Math.random()*10)+1) * 100}`
-]
+];
+
+const DEFAULT_BLACKLIST = ["reddit.com", "instagram.com", "facebook.com"];
 
 function randomLink(e) {
     chrome.storage.sync.get({'storedLinks': DEFAULT_LINKS}, (data) => {
@@ -14,9 +16,9 @@ function randomLink(e) {
 function addLinks(e) { 
     const url = document.getElementById('linkInput').value;
     chrome.storage.sync.get({'storedLinks': DEFAULT_LINKS}, (data) => {
-        storedLinks = data.storedLinks;
+        let storedLinks = data['storedLinks'];
 
-        if (url!= '') {
+        if (url !=  '') {
             if (storedLinks) {
                 storedLinks.push(url);
             } else {
@@ -31,6 +33,29 @@ function addLinks(e) {
     //window.close();
 
 }
+
+
+function addInterrupt(e) { 
+    const url = document.getElementById('linkInput').value;
+    chrome.storage.sync.get({'blackList': DEFAULT_BLACKLIST}, (data) => {
+        let newblacklist = data['blackList'];
+
+        if (url !=  '') {
+            if (newblacklist) {
+                newblacklist.push(url);
+            } else {
+                newblacklist = [url];
+            }
+        }
+        chrome.storage.sync.set({'blackList':newblacklist});
+
+        console.log(newblacklist);
+    })
+    document.getElementById('linkInput').value = '';
+    //window.close();
+}
+
+
 
 function resetLinks (e) {
     chrome.storage.sync.clear();
@@ -47,5 +72,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('resetLinks').addEventListener('click', resetLinks);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('submitInterrupt').addEventListener('click', addInterrupt);
 });
 
